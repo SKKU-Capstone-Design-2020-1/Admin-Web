cfcea0acdc49fcd6198f2acc925d45e2c70b8992
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-04-24 15:57:26 +0900

    Add an eventbusy icon to no store page

--- src/pages/main/NoStoreAdded.js
+++ src/pages/main/NoStoreAdded.js
@@ -2,12 +2,14 @@ import React from 'react'
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import EventBusyIcon from "@material-ui/icons/EventBusy"
 
 const NoStoreAdded = () => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
             <div className={classes.innerRoot}>
+                <EventBusyIcon className={classes.icon} />
                 <Typography variant="body1" gutterBottom>
                     Add your first store now!
                 </Typography>
@@ -29,6 +31,11 @@ const useStyles = makeStyles(theme => ({
     },
     innerRoot: {
         textAlign: 'center'
+    },
+    icon: {
+        height: 100,
+        width: 100,
+        marginBottom: theme.spacing(2), 
     }
 }))
 export default NoStoreAdded;
