e6234af4e2af0a314a2c8f6b676e1f3f700ab49a
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-05-05 12:08:56 +0900

    Add toolbars in map toolbar buttons

--- src/layouts/map_builder/MapToolbar.js
+++ src/layouts/map_builder/MapToolbar.js
@@ -2,34 +2,58 @@ import React from 'react'
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
-import AddIcon from "@material-ui/icons/Add";
 import SeatIcon from "@material-ui/icons/EventSeat";
 import RotateLeftIcon from "@material-ui/icons/RotateLeft";
 import RotateRightIcon from "@material-ui/icons/RotateRight";
 import EditIcon from "@material-ui/icons/Edit";
+import AddLocationIcon from "@material-ui/icons/AddLocation";
+import EditLocationIcon from "@material-ui/icons/EditLocation";
+import RemoveLocationIcon from "@material-ui/icons/LocationOff";
+import Tooltip from "@material-ui/core/Tooltip";
 
-const MapToolbar = ({handleDialog}) => {
+const MapToolbar = ({ handleDialog }) => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
             <ButtonGroup className={classes.group} size="small">
-                <Button onClick={() => handleDialog({type: 'MAP_DIALOG'})}>
-                    <AddIcon />
-                </Button>
+                <Tooltip title="Add a new map">
+                    <Button onClick={() => handleDialog({ type: 'MAP_DIALOG' })}>
+                        <AddLocationIcon />
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Edit selected map">
+                    <Button >
+                        <EditLocationIcon />
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Remove selected map">
+                    <Button>
+                        <RemoveLocationIcon />
+                    </Button>
+                </Tooltip>
             </ButtonGroup>
             <ButtonGroup className={classes.group} size="small">
-                <Button>
-                    <SeatIcon />
-                </Button>
-                <Button>
-                    <RotateLeftIcon />
-                </Button>
-                <Button>
-                    <RotateRightIcon />
-                </Button>
-                <Button>
-                    <EditIcon />
-                </Button>
+                <Tooltip title="Add a seat">
+                    <Button>
+                        <SeatIcon />
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Rotate seat to left">
+                    <Button>
+                        <RotateLeftIcon />
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Rotate seat to right">
+                    <Button>
+                        <RotateRightIcon />
+                    </Button>
+                </Tooltip>
+                <Tooltip title="Edit selected seat">
+                    <Button>
+                        <EditIcon />
+                    </Button>
+                </Tooltip>
+
             </ButtonGroup>
         </div>
     )
