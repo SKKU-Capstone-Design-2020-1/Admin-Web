f4ac0d6d4b8abc4135e7f461220e0d5adf25cda8
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-05-14 14:41:34 +0900

    Refactor bootstrapinput to different file

--- src/layouts/drawer/Drawer.js
+++ src/layouts/drawer/Drawer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles, withStyles } from "@material-ui/core/styles/";
+import { makeStyles } from "@material-ui/core/styles/";
 import Divider from "@material-ui/core/Divider";
 import MDrawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
@@ -15,42 +15,7 @@ import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-import InputBase from "@material-ui/core/InputBase";
-
-const BootstrapInput = withStyles((theme) => ({
-    root: {
-        'label + &': {
-            marginTop: theme.spacing(3),
-        },
-    },
-    input: {
-        borderRadius: 4,
-        position: 'relative',
-        backgroundColor: theme.palette.background.paper,
-        border: '1px solid #ced4da',
-        fontSize: 16,
-        padding: '10px 26px 10px 12px',
-        transition: theme.transitions.create(['border-color', 'box-shadow']),
-        // Use the system font instead of the default Roboto font.
-        fontFamily: [
-            '-apple-system',
-            'BlinkMacSystemFont',
-            '"Segoe UI"',
-            'Roboto',
-            '"Helvetica Neue"',
-            'Arial',
-            'sans-serif',
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-        ].join(','),
-        '&:focus': {
-            borderRadius: 4,
-            borderColor: '#80bdff',
-            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-        },
-    },
-}))(InputBase);
+import BootstrapInput from "./BootstrpInput";
 
 const Drawer = ({ mobileOpen, handleDrawerToggle }) => {
     const classes = useStyles();
