4ca6c79737a12d78c0b6df8fcdbc1c4e096242bd
JunKyus <34362701+kuk941025@users.noreply.github.com>
2020-06-04 00:02:13 +0900

    Initialize firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const serviceAccount = require("./config/seat-reservation-2600f-firebase-adminsdk-jllvn-0943411cf8.json");
+const cors = require('cors')({ origin: true })
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://seat-reservation-2600f.firebaseio.com"
+})
+
+const db = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+
+exports.checkReserves = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+        const reserveSnap = await db.collection(`qr_reserves`).get();
+        for (let doc of reserveSnap.docs){
+            console.log(doc.data());
+        }
+
+        return res.send("hey");
+    })
+})
+
